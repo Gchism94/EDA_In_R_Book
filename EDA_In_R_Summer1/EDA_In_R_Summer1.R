9925641e86f5cabe4b1f7283445efa94c1a347bf
Greg T. Chism <gchism@email.arizona.edu>
2022-05-09 17:23:08 -0700

    Add files via upload

--- EDA_In_R_Summer1/EDA_In_R_Summer1.R
+++ EDA_In_R_Summer1/EDA_In_R_Summer1.R
@@ -25,5 +25,56 @@ install.packages("pacman")
 # Downloads and load required packages
 pacman::p_load(dlookr,
                formattable,
+               knitr,
                nycflights13,
                tidyverse)
+
+# Objective 1.0: loading a data set and producing tables
+data("flights")
+
+# HTLM table of head(flights)
+formattable(head(flights))
+
+
+# What are the properties of the data
+formattable(diagnose(flights))
+
+# Properties of our numeric data columns
+formattable(diagnose_numeric(flights))
+
+# 2.0 Diagnosing outliers and missing values
+formattable(diagnose_outlier(flights) %>%
+  filter(outliers_ratio > 0)) 
+
+# Plotting all columns with and without outliers
+flights %>% 
+  plot_outlier()
+
+# Plotting only columns with outliers
+flights %>%
+  plot_outlier(diagnose_outlier(flights) %>%
+                 filter(outliers_ratio > 0) %>%
+                 select(variables) %>%
+                 unlist()
+  )
+
+# Selecting desired columns to plot outliers
+flights %>%
+  select(dep_delay, air_time, arr_delay) %>%
+  plot_outlier()
+
+# Categorical variables
+formattable(diagnose_category(flights))
+
+# Create the NA table
+NA.Table <- plot_na_pareto(flights, only_na = TRUE, plot = FALSE)
+
+# HTML table of NAs
+formattable(NA.Table)
+
+# Plot NA intersect of the columns with missing values
+plot_na_intersect(flights, only_na = TRUE)
+
+# 3.0 Producing an HTML summary 
+diagnose_web_report(flights)
+
